73d72c0038ebc9567703488075518c8f439c29d7
doom <doom@macbook-pro.home>
2025-12-26 03:57:05 -0500

    Add font-caveat and font-patrick to Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        caveat: ["var(--font-caveat)", "cursive"],
+        patrick: ["var(--font-patrick)", "cursive"],
+      },
     },
   },
   plugins: [],
